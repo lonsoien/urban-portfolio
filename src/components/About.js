05696a973143a1e225478ac54864bec663cf1f6c
lonsoien <79151322+lonsoien@users.noreply.github.com>
2023-01-27 17:46:47 +0100

    pages

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import Menu from './Menu';
+import Designer from '../images/Designer.jpg'; 
+
+function About () {
+    return (
+        <>
+        <Menu />
+        <h1 className='first-text'>ABOUT</h1>
+        <div>
+            <img alt='design' className='center' src={Designer}></img>
+        </div>
+        <footer>
+            <h4>by Louise Bellingmo</h4>
+        </footer>
+    </>
+  );
+}
+
+export default About; 
