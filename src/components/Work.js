05f6500b1f58f25f7031aea8e3d0ccc14f421e13
lonsoien <79151322+lonsoien@users.noreply.github.com>
2023-01-28 16:53:28 +0100

    scss styling and content

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,18 @@
-import Designer from '../images/Designer.jpg'; 
+import Environment from '../images/evironmentbox2.png'; 
 
 function Work () {
     return (
         <>
         <h1 className='first-text'>MY WORK</h1>
         <div>
-            <img alt='design' className='center' src={Designer}></img>
+            <img alt='design' className='center' src={Environment}></img>
+        </div>
+        <div className='info-text'>
+        <p>
+            Final Exam project for a real world client.
+            Environmentbox AS is a company that creates innovative storage 
+            solutions for communities and businesses. 
+        </p>
         </div>
         <footer>
             <h4>by Louise Bellingmo</h4>
