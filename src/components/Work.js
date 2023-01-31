0c4878c60ccb97c83428e701fe892893489b76ab
lonsoien <79151322+lonsoien@users.noreply.github.com>
2023-01-31 11:08:01 +0100

    scss & html

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,6 +14,31 @@ function Work () {
             solutions for communities and businesses. 
         </p>
         </div>
+        <div className="center">
+        <a href="https://evironmentbox.netlify.app/" target="blank"><button className="view">View Live</button></a>
+        &nbsp;&nbsp;
+        <a href="https://github.com/Noroff-FEU-Assignments/project-exam-2-lonsoien" target="blank"><button className="view">View Code</button></a>
+        <br></br>
+        </div>
+
+        <div>
+            <img alt='design' className='center' src={Environment}></img>
+        </div>
+        <div className='info-text'>
+        <p>
+            Final Exam project for a real world client.
+            Environmentbox AS is a company that creates innovative storage 
+            solutions for communities and businesses. 
+        </p>
+        </div>
+
+        <div className="center">
+        <a href="https://evironmentbox.netlify.app/" target="blank"><button className="view">View Live</button></a>
+        &nbsp;&nbsp;
+        <a href="https://github.com/Noroff-FEU-Assignments/project-exam-2-lonsoien" target="blank"><button className="view">View Code</button></a>
+        <br></br>
+        </div>
+        
         <footer>
             <h4>by Louise Bellingmo</h4>
         </footer>
