f3c5f7b9e2799cfa96178b878baec437de063520
lonsoien <79151322+lonsoien@users.noreply.github.com>
2023-01-31 11:35:56 +0100

    content

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,6 @@
 import Environment from '../images/evironmentbox2.png'; 
+import Kicks from '../images/kickscrew-mockup.png'; 
+import examplesite from '../images/sneakers-product.png'; 
 
 function Work () {
     return (
@@ -9,9 +11,12 @@ function Work () {
         </div>
         <div className='info-text'>
         <p>
-            Final Exam project for a real world client.
+            This is my Final Exam project at Noroff, where I chose to work for a real world client.
             Environmentbox AS is a company that creates innovative storage 
             solutions for communities and businesses. 
+            The goal was to design, plan and create a new website for a client. 
+            The client can log in to an admin page, and add their own posts for their oncoming projects 
+            and collabrations.
         </p>
         </div>
         <div className="center">
@@ -22,23 +27,30 @@ function Work () {
         </div>
 
         <div>
-            <img alt='design' className='center' src={Environment}></img>
+            <img alt='design' className='center' src={Kicks}></img>
         </div>
         <div className='info-text'>
         <p>
-            Final Exam project for a real world client.
-            Environmentbox AS is a company that creates innovative storage 
-            solutions for communities and businesses. 
+         This is a Semester Project I called Kicks Crew.
+         The goal was to create a ecommerce site with a given Strapi API from Noroff.
+         For this project I created a sneakers website with ecommerce solutions and much focus on design.
+         (sadly the site is no longer hosted and unavailable to view live).
+         <br></br>
+         Here is an example of product site.
         </p>
         </div>
+        <div>
+            <img alt='design' className='center' src={examplesite}></img>
+        </div>
+        &nbsp;&nbsp;
 
         <div className="center">
-        <a href="https://evironmentbox.netlify.app/" target="blank"><button className="view">View Live</button></a>
+        <a href="none" target="blank"><button className="view">View Live</button></a>
         &nbsp;&nbsp;
-        <a href="https://github.com/Noroff-FEU-Assignments/project-exam-2-lonsoien" target="blank"><button className="view">View Code</button></a>
+        <a href="https://github.com/lonsoien/Semester2-project" target="blank"><button className="view">View Code</button></a>
         <br></br>
         </div>
-        
+
         <footer>
             <h4>by Louise Bellingmo</h4>
         </footer>
