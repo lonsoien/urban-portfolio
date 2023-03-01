76958f5394fc6c49b1d21f3aebf2d9afe95bd724
lonsoien <79151322+lonsoien@users.noreply.github.com>
2023-03-01 10:13:34 +0100

    image update

--- urban-portfolio/src/components/Home.js
+++ urban-portfolio/src/components/Home.js
@@ -1,11 +1,12 @@
-import Designer from '../images/Designer.jpg'; 
+
+import Model from '../images/Dispersion-2.jpg'; 
 
 function Home () {
     return (
         <>
         <h1 className='first-text'>FRONTEND DEVELOPER</h1>
         <div>
-            <img alt='design' className='center' src={Designer}></img>
+            <img alt='design' className='center' src={Model}></img>
         </div>
         <footer>
             <h4>by Louise Bellingmo</h4>
